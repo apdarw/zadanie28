208e70ad9d46d9976aa968de9a747abccf0170cb
Adrian <apdarw@gmail.com>
2020-07-09 15:34:30 +0200

    Zmieniono program

--- Zadanie26.c
+++ Zadanie26.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
-#define WYSWIETLANY_ZNAK '*'
-
 int main() {
-    
-unsigned int liczba_wierszy, liczba_znakow, wiersz, kolumna; 
 
+unsigned int liczba_wierszy, liczba_znakow, wiersz, kolumna;
+char znak;
 
 printf("Podaj liczbe wierszy: ");
 scanf("%d", &liczba_wierszy);
-    
+
 printf("Podaj liczbe znakow: ");
 scanf("%d", &liczba_znakow);
-    
+
+printf("Podaj dowolny znak: ");
+scanf(" %c", &znak);
+
+printf("\n");
+
 for (wiersz = 1; wiersz <= liczba_wierszy; wiersz++) {
     for (kolumna = 1; kolumna <= liczba_znakow; kolumna++){
-        printf("%c", WYSWIETLANY_ZNAK);
+        printf("%c", znak);
     }
     printf("\n");
     }
